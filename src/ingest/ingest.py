73cda287cdf66bb57e94851ed942c1ad1a8c793f
SimonH <professional.simonhossain@gmail.com>
2026-08-03 23:54:25 +0100

    text splitter for ingest pipeline

--- src/ingest/ingest.py
+++ src/ingest/ingest.py
@@ -1,5 +1,12 @@
 from langchain import *
 from langchain_community.document_loaders import TextLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 
 loader = TextLoader("data/add_your_data.txt")
 docs = loader.load()
+
+chunks = RecursiveCharacterTextSplitter(
+    chunk_size=800,
+    chunk_overlap=120,
+).split_documents(docs)
+
