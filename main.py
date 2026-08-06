23b002f1756d563c306ef63ac4b00022c984b646
SimonH <professional.simonhossain@gmail.com>
2026-08-06 23:17:28 +0100

    moving main functions outside of app into main.py

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
+from ingest import load_context_docs, split_docs
 
 def main():
-    pass
+
+    docs = load_context_docs("data/add_your_data.txt")
+    docs = split_docs(docs)
+    print(docs)
 
 if __name__ == "__main__":
     main()
--- src/app.py
+++
@@ -1,5 +0,0 @@
-from ingest import load_context_docs, split_docs
-
-docs = load_context_docs("data/add_your_data.txt")
-docs = split_docs(docs)
-print(docs)
